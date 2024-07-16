a36aba78ffa6cd839f7acf6f6bd834b7838f3dbf
Shrenik Bhatt <44073323+shrenikbhatt@users.noreply.github.com>
2024-07-15 21:51:48 -0400

    feat: create proto definition

---
+++ proto/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod code_execution {
+    tonic::include_proto!("code_execution");
+}
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
