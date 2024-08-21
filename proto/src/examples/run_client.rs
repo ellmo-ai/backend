072e2454659eeb3ca2b92bf685d2baff6a27d45e
Shrenik Bhatt <44073323+shrenikbhatt@users.noreply.github.com>
2024-08-20 21:13:58 -0400

    chore: remove unused types

--- proto/src/examples/run_client.rs
+++ proto/src/examples/run_client.rs
@@ -1,8 +1,11 @@
+use std::{net::SocketAddr, str::FromStr};
+
 use proto::examples::dummy_client::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client: Client = Client::new().await?;
+    let addr: SocketAddr = SocketAddr::from_str("[::1]:50051").unwrap();
+    let mut client: Client = Client::new(addr).await?;
 
     let response: tonic::Response<()> = client.send_dummy_span_creation_request().await?;
     println!("RESPONSE={:?}", response);
